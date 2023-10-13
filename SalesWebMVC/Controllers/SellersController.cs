44e1feb13e7a90095077ce3975f5f664f3d3d944
Roger Ferreira <roger.fc@outlook.com>
2023-10-13 09:35:15 -0300

    Adicionando validações no preenchimento dos campos obrigatórios.

--- SalesWebMVC/Controllers/SellersController.cs
+++ SalesWebMVC/Controllers/SellersController.cs
@@ -37,7 +37,14 @@ namespace SalesWebMVC.Controllers
         [HttpPost]
         [ValidateAntiForgeryToken]
         public IActionResult Create(Seller seller)
-        {
+        {   
+            if (!ModelState.IsValid)
+            {
+                var departments = _departmentService.FindAll();
+                var viewModel = new SellerFormViewModel { Seller = seller, Departments = departments };
+                return View(viewModel);
+            }
+
             _sellerService.Insert(seller);
             return RedirectToAction(nameof(Index));
         }
@@ -107,6 +114,13 @@ namespace SalesWebMVC.Controllers
         [ValidateAntiForgeryToken]
         public IActionResult Edit(int id, Seller seller)
         {
+            if (!ModelState.IsValid)
+            {
+                var departments = _departmentService.FindAll();
+                var viewModel = new SellerFormViewModel { Seller = seller, Departments = departments };
+                return View(viewModel);
+            }
+
             if (id != seller.Id)
             {
                 return RedirectToAction(nameof(Error), new { message = "Id mismatch!" });
