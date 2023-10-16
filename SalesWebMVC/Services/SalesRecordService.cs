1f784c0fc3c623275e4cef52ac48cc20b06ce662
Roger Ferreira <roger.fc@outlook.com>
2023-10-16 16:46:02 -0300

    Implementando busca simples e agrupada de vendas.

---
+++ SalesWebMVC/Services/SalesRecordService.cs
@@ -0,0 +1,56 @@
+﻿using SalesWebMVC.Data;
+using SalesWebMVC.Models;
+using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+
+namespace SalesWebMVC.Services
+{
+    public class SalesRecordService
+    {
+        private readonly SalesWebMVCContext _context;
+
+        public SalesRecordService(SalesWebMVCContext context)
+        {
+            _context = context;
+        }
+        public List<SalesRecord> FindByDate(DateTime? minDate, DateTime? maxDate)
+        {
+            var result = from obj in _context.SalesRecord select obj;
+            if (minDate.HasValue)
+            {
+                result = result.Where(x => x.Date >= minDate.Value);
+            }
+            if (maxDate.HasValue)
+            {
+                result = result.Where(x => x.Date <= maxDate.Value);
+            }
+
+            return result
+                    .Include(x => x.Seller)
+                    .Include(x => x.Seller.Department)
+                    .OrderBy(x => x.Date)
+                    .ToList();
+        }
+        public List<IGrouping<Department,SalesRecord>> FindByDateGrouping(DateTime? minDate, DateTime? maxDate)
+        {
+            var result = from obj in _context.SalesRecord select obj;
+            if (minDate.HasValue)
+            {
+                result = result.Where(x => x.Date >= minDate.Value);
+            }
+            if (maxDate.HasValue)
+            {
+                result = result.Where(x => x.Date <= maxDate.Value);
+            }
+
+            return result
+                    .Include(x => x.Seller)
+                    .Include(x => x.Seller.Department)
+                    .OrderBy(x => x.Date)
+                    .GroupBy(x => x.Seller.Department)
+                    .ToList();
+        }
+    }
+}
